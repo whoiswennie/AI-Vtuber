fea3a4730bef205ea6d4f9176d043b5a7218a556
whoiswennie <3287305464@qq.com>
2024-08-12 16:48:54 +0800

    Initial commit for UI branch

--- condaenv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-runtime\miniconda3\Scripts\conda.exe create --name ai-vtuber python=3.10 -y
-runtime\miniconda3\envs\ai-vtuber\python.exe  -m pip install -r requirements\requirements_ai_vtuber.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-runtime\miniconda3\envs\ai-vtuber\python.exe -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-cmd /k
